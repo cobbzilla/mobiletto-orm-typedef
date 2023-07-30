effd5f0f3f948e05acc9f5920b34c9808cb1e1b5
Jonathan Cobb <jonathan@kyuss.org>
2023-07-30 12:56:57 -0400

    restrict field names

--- lib/cjs/fields.d.ts
+++ lib/cjs/fields.d.ts
@@ -1,3 +1,4 @@
 import { MobilettoOrmFieldDefConfigs } from "./field.js";
 import { MobilettoOrmTypeDef } from "./typedef.js";
+export declare const FIELD_NAME_REGEX: RegExp;
 export declare const processFields: (fields: MobilettoOrmFieldDefConfigs, objPath: string, typeDef: MobilettoOrmTypeDef) => void;
