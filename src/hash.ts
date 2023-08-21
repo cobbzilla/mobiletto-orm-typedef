274e92eb958b2d35089f2c8df47901d57c224d3c
Jonathan Cobb <jonathan@kyuss.org>
2023-08-20 21:20:32 -0400

    export some more utilities

--- src/hash.ts
+++ src/hash.ts
@@ -10,18 +10,23 @@ export const sha = (val: string | number | boolean) => shasum(`${val}`, "SHA256"
 
 export const typedefHash = (val: string | number | boolean, debug: boolean) => (debug ? val : sha(val));
 
-const insertAtIndex = (str: string, insert: string, index: number): string => {
+export const insertAtIndex = (str: string, insert: string, index: number): string => {
     return str.slice(0, index) + insert + str.slice(index);
 };
 
-export const typedefHashDirs = (val: string | number | boolean, debug: boolean, levels: number): string => {
-    if (debug) return `${val}${OBJ_DIR_SUFFIX}`;
-    let s = sha(val);
+export const shaLevels = (val: string | number | boolean, levels: number): string => {
+    let s = sha(`${val}`);
     if (levels > 0) {
         for (let i = 0; i < levels; i++) {
             s = insertAtIndex(s, "/", 2 + 3 * i);
         }
     }
+    return s;
+};
+
+export const typedefHashDirs = (val: string | number | boolean, debug: boolean, levels: number): string => {
+    if (debug) return `${val}${OBJ_DIR_SUFFIX}`;
+    const s = shaLevels(val, levels);
     return s + OBJ_DIR_SUFFIX;
 };
 
